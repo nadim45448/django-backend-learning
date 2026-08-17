bf2e3065d1621a7c6c67dc1221474e921ce8baae
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2026-08-17 16:27:09 +0600

    return json response from django

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name ='index'),
     path('hello/<str:name>/', views.hello_user, name='hello_user'),
+    path('products/',views.product_list, name='product_list'),
+    path('products/<int:product_id>/', views.product_details, name='product_details'),
+   
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
@@ -29,3 +29,32 @@ def hello_user(request, name):
     return HttpResponse(
         f"Hello, {name}!"
     )
+
+# returning json from django
+def product_details(request, product_id):
+    return JsonResponse({
+        "id":product_id,
+        "name":"Laptop",
+        "price":800
+    })
+
+def product_list(request):
+    products_list = [
+        {
+            "id":1,
+            "name":"Laptop",
+            "price":900
+        },
+        {
+            "id":2,
+            "name":"Keyboard",
+            "price":150
+        },
+        {
+            "id": 3,
+            "name": "Mouse",
+            "price": 50
+        }
+    ]
+    return JsonResponse(products_list, safe=False)
+    
