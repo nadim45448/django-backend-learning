ed331553c4903bb7f50bb1ff9c002259b328a291
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2026-08-17 15:02:30 +0600

    inspect request object

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1> Welcome to django learning </h1>')
+    # return HttpResponse('<h1> Welcome to django learning </h1>')
+
+    # inspact request object
+    return HttpResponse(
+        f"Method: {request.method}<br>"
+        f"Path: {request.path}<br>"
+        f"User Agent: {request.headers.get('User-Agent')}<br>"
+    )
+
 
 def hello(request):
     return HttpResponse("Hello from Django!")
