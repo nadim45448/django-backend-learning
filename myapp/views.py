60b2e1a19e08128b0b49a5162cefa26f579fcdaa
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2026-08-17 15:04:32 +0600

    query parameter

--- myapp/views.py
+++ myapp/views.py
@@ -7,10 +7,17 @@ def index(request):
     # return HttpResponse('<h1> Welcome to django learning </h1>')
 
     # inspact request object
+    # return HttpResponse(
+    #     f"Method: {request.method}<br>"
+    #     f"Path: {request.path}<br>"
+    #     f"User Agent: {request.headers.get('User-Agent')}<br>"
+    # )
+
+    # query parameter
+    name = request.GET.get('name')
+    age = request.GET.get('age')
     return HttpResponse(
-        f"Method: {request.method}<br>"
-        f"Path: {request.path}<br>"
-        f"User Agent: {request.headers.get('User-Agent')}<br>"
+        f"Hello {name}, you are {age} years old."
     )
 
 
