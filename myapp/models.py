d0d16ac9a310c7bb0721253a47a5e7afdab2d9c6
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2026-08-20 19:11:29 +0600

    model relationship

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,25 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='orders'
+    )
+
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name='orders'
+    )
