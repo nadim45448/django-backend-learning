ff7dc11f5d0aea43daa9ff2349549f560e9b29be
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2026-08-22 01:53:33 +0600

    drf viewset

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,20 +1,27 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index, name ='index'),
-    path('hello/<str:name>/', views.hello_user, name='hello_user'),
-    path('products/', views.products, name="products"),
-    path('products/<int:product_id>/', views.product_details, name='product_details'),
-    path('text/',views.text_input, name ="text_input"),
-    path('counter/',views.counter, name="counter"),
-    path('items/',views.items, name="items"),
-    path('items/<int:item_id>/',views.item_details, name="item_details"),
-    path('orders/',views.orders, name="orders"),
-    path('item/create/',views.item_create, name="item_create"),
-    path('item/list/',views.item_list, name="item_list"),
-    # path('item/',views.item, name="item"),
-    path('item/',views.ItemListView.as_view(), name="item"),
-    # path('item/<int:item_id>/',views.items_details, name="items_details"),
-    path('item/<int:item_id>/',views.ItemDetailView.as_view(), name="items_detail"),
-]
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
+
+# urlpatterns = [
+#     path('', views.index, name ='index'),
+#     path('hello/<str:name>/', views.hello_user, name='hello_user'),
+#     path('products/', views.products, name="products"),
+#     path('products/<int:product_id>/', views.product_details, name='product_details'),
+#     path('text/',views.text_input, name ="text_input"),
+#     path('counter/',views.counter, name="counter"),
+#     path('items/',views.items, name="items"),
+#     path('items/<int:item_id>/',views.item_details, name="item_details"),
+#     path('orders/',views.orders, name="orders"),
+#     path('item/create/',views.item_create, name="item_create"),
+#     path('item/list/',views.item_list, name="item_list"),
+#     # path('item/',views.item, name="item"),
+#     path('item/',views.ItemListView.as_view(), name="item"),
+#     # path('item/<int:item_id>/',views.items_details, name="items_details"),
+#     path('item/<int:item_id>/',views.ItemDetailView.as_view(), name="items_detail"),
+# ]
+
+router = DefaultRouter()
+router.register('item', ItemViewSet, basename='item')
+urlpatterns = router.urls
--- myapp/views.py
+++ myapp/views.py
@@ -12,6 +12,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ViewSet
 
 from .serializers import ProductSerializer
 
@@ -453,3 +454,40 @@ class ItemDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_url_kwarg = 'item_id'
+
+# viewsets
+class ItemViewSet(ViewSet):
+    def list(self, request):
+        items = Product.objects.all()
+        serializer = ProductSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        item = get_object_or_404(Product, id=pk)
+        serializer = ProductSerializer(item)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        item = get_object_or_404(Product, id=pk)
+        serializer = ProductSerializer(item, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_update(self, request, pk=None):
+        item = get_object_or_404(Product, id=pk)
+        serializer = ProductSerializer(item, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        item = get_object_or_404(Product, id=pk)
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
